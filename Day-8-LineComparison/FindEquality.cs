78c26d0bdc41b1eac2391944f0bca5bf5a486b59
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-01 16:12:57 +0530

    Added: UC2_FindEquality

---
+++ Day-8-LineComparison/FindEquality.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_LineComparison
+{
+    internal class FindEquality
+    {
+        public void EqualityMethod(double len1, double len2)
+        {
+            if (len1.Equals(len2))
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
+        }
+    }
+}
--- Day-8-LineComparison/Program.cs
+++ Day-8-LineComparison/Program.cs
@@ -2,5 +2,16 @@
 using Day_8_LineComparison;
 
 Console.WriteLine("----- Welcome TO Line Calculations -----\n");
-FindLength obj = new FindLength();
-Console.WriteLine("\nLength of of given line :- "+ obj.FindingLength());
+Console.WriteLine("\nEnter the co-ordinates for Line 1:-\n");
+FindLength obj1 = new FindLength();
+double len1 = obj1.FindingLength();
+Console.WriteLine("\nLength of line 1 :- "+len1);
+
+Console.WriteLine("\nEnter the co-ordinates for Line 2:-\n");
+FindLength obj2= new FindLength();
+double len2 = obj2.FindingLength();
+Console.WriteLine("\nLength of line 2 :- " +len2);
+
+FindEquality obj3 = new FindEquality();
+Console.Write("\nResult:- "); // console.write don't print next statement in new line 
+obj3.EqualityMethod(len1, len2);
