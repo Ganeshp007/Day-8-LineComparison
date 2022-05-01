a511eaaaa97b25bd0c184f8ac371c46e96afed82
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-01 13:53:16 +0530

    Added: UC1_FindLineLength

---
+++ Day-8-LineComparison/FindLength.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_LineComparison
+{
+    internal class FindLength
+    {
+        public int len1;
+        public int len2;
+        public int x1;
+        public int y1;
+        public int x2;
+        public int y2;
+
+
+        public FindLength()//Constructor
+        { 
+            double[] StoringLength = new double[5];
+            for (int i = 1; i < 2; i++)
+            {
+                Console.WriteLine("Enter value for  coordinate x1");
+                this.x1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter value for   coordinate y1");
+                this.y1 = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("Enter value for  first coordinate x2");
+                this.x2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter value for  first coordinate y2");
+                this.y2 = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public double FindingLength()//methd to calculate length
+        {
+         
+            double LineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double len = Math.Round(LineLength, 2);
+            return len;
+
+        }
+    }
+}
---
+++ Day-8-LineComparison/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day_8_LineComparison;
+
+Console.WriteLine("----- Welcome TO Line Calculations -----\n");
+FindLength obj = new FindLength();
+Console.WriteLine("\nLength of of given line :- "+ obj.FindingLength());
